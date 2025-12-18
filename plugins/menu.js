4d7ba6326f7962319e50896b2f14b4a7a20e7fe1
Njabulo Jb <fanajb65@gmail.com>
2025-12-18 22:59:41 +0200

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -128,19 +128,7 @@ const buttons = [
               renderLargerThumbnail: false,        
             }
           }
-        }, { quoted: {
-            key: {
-                fromMe: false,
-                participant: `0@s.whatsapp.net`,
-                remoteJid: "status@broadcast"
-            },
-            message: {
-                contactMessage: {
-                    displayName: "njᥲbᥙᥣo",
-                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=26777821911:+26777821911\nitem1.X-ABLabel:Bot\nEND:VCARD`
-                }
-            }
-        } });
+      }, { quoted: msg })
 
 
     } catch (e) {
