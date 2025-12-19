36ad8b4444ad25ba7543ceb00ce672dea696d5c0
Njabulo Jb <fanajb65@gmail.com>
2025-12-19 11:21:25 +0200

    Update apk.js

--- plugins/apk.js
+++ plugins/apk.js
@@ -38,6 +38,42 @@ module.exports = {
       const buttons = [
   { buttonId: '.web',   buttonText: { displayText: '🍬sᴇʟғs ғᴀᴍɪʟʏ' },   type: 1 },
 ]; 
+              const apkMsg =`╭─⃝──────⊷
+*┊ ┊ ┊ ┊ ┊ ┊┊* 
+*┊ ┊ ✫ ˚㋛ ⋆｡ ❀ ✧* 
+*┊ ☪︎⋆*
+*⊹*    🪔 *𝐌𝐄𝐍𝐔*
+*✧* 「message *: apk* 」
+
+*┊* ✧ _*ping test time*_
+┊ ──¬¬¬¬¦
+┊▢ɴᴀᴍᴇ : ɴᴊᴀʙᴜʟᴏ ᴊʙ ᴇʟɪᴛᴇ
+┊▢ᴘing : ${latency}ms
+┊▢ɴᴏᴅᴇ ᴠᴇʀsɪᴏɴ : ᴠ2.1.3
+┊ ──¬¬¬¬¬¦
+┊ *ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ ғʀᴇᴇ ғᴀᴍɪʟʏ*
+╰┬──────────⊷⳹`;
+
+        const buttons = [
+  { buttonId: '.web',   buttonText: { displayText: '🍬sᴇʟғs ғᴀᴍɪʟʏ' },   type: 1 },
+];
+      await sock.sendMessage(from,{
+        image: { url: 'https://files.catbox.moe/xazdqk.jpg' },
+          caption: apkMsg,
+          buttons: buttons
+              }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "njᥲbᥙᥣo",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=26777821911:+26777821911\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
       
       // Send APK
       await socket.sendMessage(sender, {
