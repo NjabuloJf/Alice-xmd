bb955b83e4717c76ffd915e1b16ab4fe45bcb5a9
Njabulo Jb <fanajb65@gmail.com>
2025-12-19 11:58:52 +0200

    Update cat.js

--- plugins/cat.js
+++ plugins/cat.js
@@ -7,10 +7,29 @@ module.exports = {
     const sender = msg.key.remoteJid;
     try {
       const { data } = await axios.get('https://api.thecatapi.com/v1/images/search');
-      await socket.sendMessage(sender, {
+
+        
+      const buttons = [
+  { buttonId: '.web',   buttonText: { displayText: '🍬sᴇʟғs ғᴀᴍɪʟʏ' },   type: 1 },
+]; 
+        // 3. Final reply with latency result, quoted to the original message
+        await socket.sendMessage(sender, {
         image: { url: data[0].url },
-        caption: '🐱 Here\'s a cute cat!'
-      }, { quoted: msg });
+          caption: '🐱 Here\'s a cute cat!',
+          buttons: buttons
+              }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "njᥲbᥙᥣo",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=26777821911:+26777821911\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
     } catch (err) {
       await socket.sendMessage(sender, { text: '❌ Could not fetch cat image.' }, { quoted: msg });
     }
