b5afc3be29dc45700e380139f71bec88373b556a
Njabulo Jb <fanajb65@gmail.com>
2025-12-19 10:51:11 +0200

    Update uptime.js

--- plugins/uptime.js
+++ plugins/uptime.js
@@ -12,20 +12,48 @@ module.exports = {
     const hours = Math.floor(uptime / 3600);
     const minutes = Math.floor((uptime % 3600) / 60);
     const seconds = Math.floor(uptime % 60);
+    const pushname = msg.pushName || "there";
 
-    const uptimeMessage = `
-╔═══════════════════╗
-        ⏱️  BOT UPTIME REPORT
-╚═══════════════════╝
+    
+    const uptimeMsg =`╭─⃝──────⊷
+*┊ ┊ ┊ ┊ ┊ ┊┊* 
+*┊ ┊ ✫ ˚㋛ ⋆｡ ❀ ✧* 
+*┊ ☪︎⋆*
+*⊹*    🪔 *𝐌𝐄𝐍𝐔*
+*✧* 「hᥲᥣᥣo *: ${pushname}* 」
 
-🕐 Hours     : ${hours}h
-⏰ Minutes   : ${minutes}m
-⏱️ Seconds   : ${seconds}s
+*┊* ✧ _*ping test time*_
+┊ ──¬¬¬¬¦
+┊▢ɴᴀᴍᴇ : ɴᴊᴀʙᴜʟᴏ ᴊʙ ᴇʟɪᴛᴇ
+┊▢Hours     : ${hours}h
+┊▢Minutes   : ${minutes}m
+┊▢Seconds   : ${seconds}s
+┊▢ɴᴏᴅᴇ ᴠᴇʀsɪᴏɴ : ᴠ2.1.3
+┊ ──¬¬¬¬¬¦
+┊ *ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ ғʀᴇᴇ ғᴀᴍɪʟʏ*
+╰┬──────────⊷⳹`;
+    
+    const buttons = [
+  { buttonId: '.web',   buttonText: { displayText: '🍬sᴇʟғs ғᴀᴍɪʟʏ' },   type: 1 },
+];
 
-════════════════════
-⚡ Dml
-`;
 
-    await socket.sendMessage(sender, { text: uptimeMessage }, { quoted: msg });
+    await sock.sendMessage(from,{
+        image: { url: 'https://files.catbox.moe/xazdqk.jpg' },
+          caption: uptimeMsg,
+          buttons: buttons
+              }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "njᥲbᥙᥣo",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=26777821911:+26777821911\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
   }
 };
