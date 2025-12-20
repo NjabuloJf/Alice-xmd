6f9e9804ae2470a298bec0f5270b7d572a4f21ae
Njabulo Jb <fanajb65@gmail.com>
2025-12-20 12:06:33 +0200

    Modify ping response format and button label
    Updated the ping response message and button text.

--- plugins/ping.js
+++ plugins/ping.js
@@ -46,13 +46,12 @@ module.exports = {
 ┊ ──¬¬¬¬¦
 ┊▢ɴᴀᴍᴇ : ɴᴊᴀʙᴜʟᴏ ᴊʙ ᴇʟɪᴛᴇ
 ┊▢ᴘing : ${latency}ms
-┊▢ɴᴏᴅᴇ ᴠᴇʀsɪᴏɴ : ᴠ2.1.3
 ┊ ──¬¬¬¬¬¦
 ┊ *ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ ғʀᴇᴇ ғᴀᴍɪʟʏ*
 ╰┬──────────⊷⳹`;
 
         const buttons = [
-  { buttonId: '.web',   buttonText: { displayText: '🍬sᴇʟғs ғᴀᴍɪʟʏ' },   type: 1 },
+  { buttonId: '.web',   buttonText: { displayText: '⏲️Self family ping' },   type: 1 },
 ];
 
         // 3. Final reply with latency result, quoted to the original message
